d025d8a87a0ae4017aa394d5ab40d12fa9282c50
Olivia0414 <zhangshuyue0414@gmail.com>
2022-02-02 16:29:33 +0100

    Update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,3 +1,4 @@
+import collections
 from . import Expense
 import matplotlib.pyplot as plt
 
@@ -5,7 +6,19 @@ def main():
     expenses = Expense.Expenses()
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
+    spending_categories = []
+    for expense in expenses.list:
+        spending_categories.append(expense.category)
+    spending_counter = collections.Counter(spending_categories)
+    print(spending_counter)
 
+    top5 = spending_counter.most_common(5)
+    categories, count = zip(*top5)
+
+    fig, ax = plt.subplots()
+    ax.bar(categories, count)
+    ax.set_title('# of Purchases by Category')
+    plt.show()
 
 if __name__ == "__main__":
     main()
